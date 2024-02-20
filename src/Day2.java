87437646d3bee334140f573823e6b1e295dabf64
Avish <avitmr2345@gmail.com>
2024-02-20 22:22:03 +0530

    2.iii

--- src/Day2.java
+++ src/Day2.java
@@ -2,8 +2,13 @@ public class Day2 {
     public static void main(String[] args) {
         // int[] arr = {8,4,2};
         // System.out.println(isPossibleToMakeAscending(arr));
-        int[] arr = {1,7,3,6,5,6};
-        System.out.println(arrayEquilibriumIndex(arr));
+
+        // int[] arr = {1,7,3,6,5,6};
+        // System.out.println(arrayEquilibriumIndex(arr));
+
+        int[] arr = {0,1,2,3,4,5,6};
+        System.out.println(firstMissing(arr, arr.length));
+
     }
 
     public static boolean isPossibleToMakeAscending(int[] arr) {
@@ -20,6 +25,7 @@ public class Day2 {
         }
         return count <= 1;
     }
+
     public static int arrayEquilibriumIndex(int[] arr) {
         int rightsum = 0;
         for (int i : arr)
@@ -33,5 +39,28 @@ public class Day2 {
             leftsum += arr[i];
         }
         return -1;
+    }
+
+    public static int firstMissing(int[] arr, int n) {
+        int i = 0;
+        while (i < n) {
+            while (arr[i] != i) {
+                if (arr[i] < 0 || arr[i] >= n || arr[arr[i]] == arr[i])
+                    break;
+                int temp = arr[arr[i]];
+                arr[arr[i]] = arr[i];
+                arr[i] = temp;
+            }
+            i++;
+        }
+        for (int j = 1; j < n; j++) {
+            if (arr[j] != j) {
+                return j;
+            }
+        }
+        if (arr[0] == n)
+            return n + 1;
+
+        return n;
     }
 }
