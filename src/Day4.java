fccc87999cac9016e08cead2400290a62eb0c0a6
Avish <avitmr2345@gmail.com>
2024-02-23 18:59:28 +0530

    4.1

---
+++ src/Day4.java
@@ -0,0 +1,23 @@
+public class Day4 {
+    public static void main(String[] args) {
+        int[][] mat = {{1,0}, {0,1}};
+        System.out.println(coverageOfMatrix(mat));
+    }
+    public static Integer coverageOfMatrix(int[][] mat) {
+		int row = mat.length;
+		int col = mat[0].length;
+		
+		Integer ans = 0;
+		for(int i = 0; i < row; i++){
+			for(int j = 0; j < col; j++){
+				if(mat[i][j] == 0){
+					if(i-1 >=0 && mat[i-1][j] == 1) ans++;
+					if(i+1 < row && mat[i+1][j] == 1) ans++;
+					if(j-1 >=0 && mat[i][j-1] == 1) ans++;
+					if(j+1 < col && mat[i][j+1] == 1) ans++;
+				}
+			}
+		}
+		return ans;
+	}
+}
