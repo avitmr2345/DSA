34b887b83da3357844a257656fea6590968b9b9a
Avish <avitmr2345@gmail.com>
2024-02-23 21:13:54 +0530

    4.ii

--- src/Day4.java
+++ src/Day4.java
@@ -1,7 +1,10 @@
 public class Day4 {
     public static void main(String[] args) {
-        int[][] mat = {{1,0}, {0,1}};
-        System.out.println(coverageOfMatrix(mat));
+        // int[][] mat = {{1,0}, {0,1}};
+        // System.out.println(coverageOfMatrix(mat));
+
+        int[][] matrix = {{1,2,3},{2,4,5},{3,5,8}};
+        System.out.println(isMatrixSymmetric(matrix));
     }
     public static Integer coverageOfMatrix(int[][] mat) {
 		int row = mat.length;
@@ -20,4 +23,16 @@ public class Day4 {
 		}
 		return ans;
 	}
+    public static boolean isMatrixSymmetric(int[][] matrix) {
+        int row = matrix.length;
+        int col = matrix[0].length;
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (matrix[i][j] != matrix[j][i]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 }
