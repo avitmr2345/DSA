9863ac861f24b3df87324c561650c15c8433a5d6
Avish <avitmr2345@gmail.com>
2024-09-17 17:36:12 +0530

    pattern 10 solved

--- src/Patterns.java
+++ src/Patterns.java
@@ -2,6 +2,7 @@ public class Patterns {
     public static void main(String[] args) {
         pattern7(8);
         pattern8(8);
+        pattern10(8);
     }
 
     private static void pattern7(int n) {
@@ -34,6 +35,18 @@ public class Patterns {
                 System.out.print("*");
             }
             counter--;
+            System.out.println();
+        }
+    }
+
+    private static void pattern10(int n) {
+        for (int i = 1; i <= 2 * n - 1; i++) {
+            int star = i;
+            if (i > n)
+                star = 2 * n - i;
+            for (int j = 1; j <= star; j++) {
+                System.out.print("*");
+            }
             System.out.println();
         }
     }
