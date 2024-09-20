7c44a70d0903be731b3ebc8996d566e42f2dedd7
Avish <avitmr2345@gmail.com>
2024-09-20 20:32:59 +0530

    pattern 20 solved

--- src/Patterns.java
+++ src/Patterns.java
@@ -9,6 +9,7 @@ public class Patterns {
         pattern17(6);
         pattern18(5);
         pattern19(5);
+        pattern20(6);
     }
 
     private static void pattern7(int n) {
@@ -164,6 +165,31 @@ public class Patterns {
             }
             spaces -= 2;
             for (int l = 1; l <= x; l++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    private static void pattern20(int n) {
+        int spaces = 2 * n - 2;
+        for (int i = 1; i <= 2 * n - 1; i++) {
+            int stars = i;
+            if (i > n)
+                stars = 2 * n - i;
+            for (int j = 1; j <= stars; j++) {
+                System.out.print("*");
+            }
+
+            for (int k = 1; k <= spaces; k++) {
+                System.out.print(" ");
+            }
+            if (i < n) {
+                spaces -= 2;
+            } else
+                spaces += 2;
+
+            for (int l = 1; l <= stars; l++) {
                 System.out.print("*");
             }
             System.out.println();
