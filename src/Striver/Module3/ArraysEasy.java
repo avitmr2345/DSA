f6aa182d48c07c28c4e29447d99b74d7932a8c72
Avish <avitmr2345@gmail.com>
2024-11-30 15:56:55 +0530

    union of two sorted arrays solved

--- src/Striver/Module3/ArraysEasy.java
+++ src/Striver/Module3/ArraysEasy.java
@@ -1,10 +1,15 @@
 package Striver.Module3;
 
+import java.util.ArrayList;
+
 public class ArraysEasy {
 
     public static void main(String[] args) {
         int[] arr = { 1, 2, 4, 7, 7, 5 };
         SecondLargestSecondSmallest(arr);
+        int[] arr1 = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        int[] arr2 = { 2, 3, 4, 4, 5, 11, 12 };
+        System.out.println(Union(arr1, arr2, arr1.length, arr2.length));
     }
 
     public static void SecondLargestSecondSmallest(int[] arr) {
@@ -43,5 +48,35 @@ public class ArraysEasy {
             System.out.println("-1");
         else
             System.out.println("Second smallest is: " + smin);
+    }
+
+    public static ArrayList<Integer> Union(int[] arr1, int[] arr2, int n, int m) {
+        int i = 0;
+        int j = 0;
+        ArrayList<Integer> unionArr = new ArrayList<>();
+        while (i < n && j < m) {
+            if (arr1[i] <= arr2[j]) {
+                if (unionArr.size() == 0 || unionArr.get(unionArr.size() - 1) != arr1[i])
+                    unionArr.add(arr1[i]);
+                i++;
+            } else {
+                if (unionArr.size() == 0 || unionArr.get(unionArr.size() - 1) != arr2[j])
+                    unionArr.add(arr2[j]);
+                j++;
+            }
+        }
+
+        for (int x = i; x < n; x++) {
+            if (!unionArr.contains(arr1[x])) {
+                unionArr.add(arr1[x]);
+            }
+        }
+
+        for (int y = j; y < m; y++) {
+            if (!unionArr.contains(arr2[y])) {
+                unionArr.add(arr2[y]);
+            }
+        }
+        return unionArr;
     }
 }
