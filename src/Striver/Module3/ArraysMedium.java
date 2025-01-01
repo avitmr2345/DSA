2777d95faa740d84553e903f8939ae7144b0c62f
Avish <avitmr2345@gmail.com>
2025-01-01 21:28:14 +0530

    updated

--- src/Striver/Module3/ArraysMedium.java
+++ src/Striver/Module3/ArraysMedium.java
@@ -332,7 +332,7 @@ public class ArraysMedium {
     public static int findAllSubarraysWithGivenSum(int arr[], int k) {
         int n = arr.length;
         HashMap<Integer, Integer> mpp = new HashMap<>();
-        int preSum = 0, cnt = 0;
+        int preSum = 0, count = 0;
 
         mpp.put(0, 1);
         for (int i = 0; i < n; i++) {
@@ -340,10 +340,10 @@ public class ArraysMedium {
 
             int remove = preSum - k;
 
-            cnt += mpp.getOrDefault(remove, 0);
+            count += mpp.getOrDefault(remove, 0);
 
             mpp.put(preSum, mpp.getOrDefault(preSum, 0) + 1);
         }
-        return cnt;
+        return count;
     }
 }
