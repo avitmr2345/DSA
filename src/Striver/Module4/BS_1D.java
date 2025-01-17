5ce9150f09f102a24e89d1f705aad8fc2dff49c0
Avish <avitmr2345@gmail.com>
2025-01-17 19:07:09 +0530

    Search Insert Position

--- src/Striver/Module4/BS_1D.java
+++ src/Striver/Module4/BS_1D.java
@@ -4,6 +4,7 @@ public class BS_1D {
     public static void main(String[] args) {
         System.out.println(lowerBound(new int[] { 3, 9, 34, 39, 43, 94, 99 }, 40));
         System.out.println(upperBound(new int[] { 3, 5, 8, 9, 15, 19 }, 7));
+        System.out.println(searchInsert(new int[] { 1, 2, 4, 7 }, 6));
     }
 
     public static int lowerBound(int[] arr, int x) {
@@ -36,6 +37,25 @@ public class BS_1D {
                 index = mid + 1;
                 s = mid + 1;
             } else {
+                e = mid - 1;
+            }
+        }
+        return index;
+    }
+
+    public static int searchInsert(int[] arr, int x) {
+        int index = arr.length;
+
+        int s = 0, e = arr.length - 1;
+        while (s <= e) {
+            int mid = (s + e) / 2;
+
+            if (x == arr[mid]) {
+                return mid;
+            } else if (x > arr[mid]) {
+                s = mid + 1;
+            } else {
+                index = mid;
                 e = mid - 1;
             }
         }
