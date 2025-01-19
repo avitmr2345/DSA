255529320812d8300fd1cf7b8e95aed339880aa1
Avish <avitmr2345@gmail.com>
2025-01-19 21:03:40 +0530

    Search Element in a Rotated Sorted Array

--- src/Striver/Module4/BS_1D.java
+++ src/Striver/Module4/BS_1D.java
@@ -8,6 +8,7 @@ public class BS_1D {
         findFloor(new int[] { 3, 4, 4, 7, 8, 10 }, 5);
         System.out.println(lastOccurrence(new int[] { 3, 4, 13, 13, 13, 20, 40 }, 13));
         System.out.println(countOccurrence(new int[] { 2, 2, 3, 3, 3, 3, 4 }, 1));
+        System.out.println(searchElementRotatedArray(new int[] { 4, 5, 6, 7, 0, 1, 2, 3 }, 0));
     }
 
     public static int lowerBound(int[] arr, int x) {
@@ -143,5 +144,28 @@ public class BS_1D {
             }
         }
         return (last - first + 1);
+    }
+
+    public static int searchElementRotatedArray(int[] arr, int target) {
+        int s = 0, e = arr.length - 1;
+
+        while (s <= e) {
+            int mid = (s + e) / 2;
+
+            if (target == arr[mid]) {
+                return mid;
+            } else if (arr[s] <= arr[mid]) {
+                if (target >= arr[s] && target <= arr[mid])
+                    e = mid - 1;
+                else
+                    s = mid + 1;
+            } else {
+                if (target >= arr[mid] && target <= arr[e])
+                    s = mid + 1;
+                else
+                    e = mid - 1;
+            }
+        }
+        return -1;
     }
 }
