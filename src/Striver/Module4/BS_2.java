e3630d80f3428ebe2f1451dda87cfdcdb298d12a
Avish <avitmr2345@gmail.com>
2025-01-21 22:12:03 +0530

    Nth Root of a Number using Binary Search

--- src/Striver/Module4/BS_2.java
+++ src/Striver/Module4/BS_2.java
@@ -3,7 +3,7 @@ package Striver.Module4;
 public class BS_2 {
     public static void main(String[] args) {
         System.out.println(findSquareRoot(28));
-
+        System.out.println(findNthRoot(27, 3));
     }
 
     public static long findSquareRoot(long n) {
@@ -21,5 +21,35 @@ public class BS_2 {
             }
         }
         return ans;
+    }
+
+    public static int power(int mid, int pow, int n) {
+        long ans = 1;
+        for (int i = 1; i <= pow; i++) {
+            ans = ans * mid;
+        }
+        if (ans > n)
+            return 2;
+        if (ans == n)
+            return 1;
+        return 0;
+    }
+
+    public static int findNthRoot(int n, int pow) {
+        int s = 1, e = n;
+
+        while (s <= e) {
+            int mid = s + (e - s) / 2;
+            int multiplication = power(mid, pow, n);
+
+            if (multiplication == 1) {
+                return mid;
+            } else if (multiplication == 0) {
+                s = mid + 1;
+            } else {
+                e = mid - 1;
+            }
+        }
+        return -1;
     }
 }
