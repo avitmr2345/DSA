03b8c80c1cb9af92f32290ac321e87956fee585d
Avish <avitmr2345@gmail.com>
2025-02-01 21:52:11 +0530

    Search in a sorted 2D matrix

--- src/Striver/Module4/BS_2D.java
+++ src/Striver/Module4/BS_2D.java
@@ -4,6 +4,8 @@ public class BS_2D {
     public static void main(String[] args) {
         int[][] arr = { { 1, 1, 1 }, { 0, 0, 1 }, { 0, 0, 0 } };
         System.out.println(findRowWithMaxOne(arr));
+        int[][] arr2 = { { 1, 2, 3, 4 }, { 5, 6, 7, 8 }, { 9, 10, 11, 12 } };
+        System.out.println(findElement(arr2, 8));
     }
 
     public static int findRowWithMaxOne(int[][] arr) {
@@ -18,5 +20,25 @@ public class BS_2D {
             }
         }
         return row;
+    }
+
+    public static boolean findElement(int[][] arr, int target) {
+        int n = arr.length;
+        int m = arr[0].length;
+
+        int s = 0, e = n * m - 1;
+        while (s <= e) {
+            int mid = (s + e) / 2;
+            int row = mid / m, col = mid % m;
+
+            if (arr[row][col] == target) {
+                return true;
+            } else if (arr[row][col] < target) {
+                s = mid + 1;
+            } else {
+                e = mid - 1;
+            }
+        }
+        return false;
     }
 }
