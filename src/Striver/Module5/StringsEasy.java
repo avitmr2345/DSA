f262dd89a942a696f5ccbb18955a0fb665a76f2b
Avish <avitmr2345@gmail.com>
2025-05-12 18:22:25 +0530

    Isomorphic String

--- src/Striver/Module5/StringsEasy.java
+++ src/Striver/Module5/StringsEasy.java
@@ -10,6 +10,7 @@ class StringsEasy {
         System.out.println(largestOddNumber("23953767242388496965328710"));
         String[] arr1 = { "flower", "flow", "flight" };
         System.out.println(longestCommonPrefix(arr1));
+        System.out.println(isIsomorphic("foo", "bar"));
     }
 
     public static String removeOuterParentheses(String s) { // *
@@ -69,5 +70,22 @@ class StringsEasy {
             ans.append(first.charAt(i));
         }
         return ans.toString();
+    }
+
+    public static boolean isIsomorphic(String s, String t) { // *
+        int map1[] = new int[200];
+        int map2[] = new int[200];
+
+        if (s.length() != t.length())
+            return false;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (map1[s.charAt(i)] != map2[t.charAt(i)])
+                return false;
+
+            map1[s.charAt(i)] = i + 1;
+            map2[t.charAt(i)] = i + 1;
+        }
+        return true;
     }
 }
