1f9064f94efa419ba3efbd2f806abf13ca59e634
Avish <avitmr2345@gmail.com>
2025-05-13 20:51:34 +0530

    Implement Atoi (String to Integer)

--- src/Striver/Module5/StringsMedium.java
+++ src/Striver/Module5/StringsMedium.java
@@ -12,6 +12,7 @@ public class StringsMedium {
         System.out.println(frequencySort("Aabb"));
         System.out.println(maxDepth("(1+(2*3)+((8)/4))+1"));
         System.out.println(romanToInt("MCMXCIV"));
+        System.out.println(myAtoi("  0000000000012345678"));
     }
 
     public static String frequencySort(String s) { // *
@@ -65,5 +66,59 @@ public class StringsMedium {
             }
         }
         return ans;
+    }
+
+    public static int myAtoi(String s) {
+        StringBuilder sb = new StringBuilder();
+        int index = 0;
+        s = s.trim();
+
+        if (s.isEmpty())
+            return 0;
+
+        if (s.charAt(0) == '-') {
+            sb.append(s.charAt(0));
+            index = 1;
+        } else if (s.charAt(0) == '+') {
+            index = 1;
+        }
+
+        for (int i = index; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (ch == '0') {
+                if (sb.length() == 1 && sb.charAt(0) == '-') {
+                    continue;
+                } else if (sb.length() == 0) {
+                    continue;
+                } else {
+                    sb.append(ch);
+                }
+            } else if (Character.isDigit(ch)) {
+                sb.append(ch);
+            } else {
+                break;
+            }
+        }
+
+        String numStr = sb.toString();
+        if (numStr.equals("-") || numStr.isEmpty()) {
+            return 0;
+        }
+
+        String maxIntStr = String.valueOf(Integer.MAX_VALUE);
+        String minIntStr = String.valueOf(Integer.MIN_VALUE);
+
+        if (numStr.charAt(0) == '-') {
+            if (numStr.length() > minIntStr.length() ||
+                    (numStr.length() == minIntStr.length() && numStr.compareTo(minIntStr) > 0)) {
+                return Integer.MIN_VALUE;
+            }
+        } else {
+            if (numStr.length() > maxIntStr.length() ||
+                    (numStr.length() == maxIntStr.length() && numStr.compareTo(maxIntStr) > 0)) {
+                return Integer.MAX_VALUE;
+            }
+        }
+        return Integer.parseInt(numStr);
     }
 }
