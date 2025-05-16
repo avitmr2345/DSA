2cae7411adaebd3f9a908735e66117936d4f3ffe
Avish <avitmr2345@gmail.com>
2025-05-16 15:54:34 +0530

    Find the starting point in LL

--- src/Striver/Module6/CustomLinkedList.java
+++ src/Striver/Module6/CustomLinkedList.java
@@ -251,4 +251,41 @@ public class CustomLinkedList {
         }
         return 0;
     }
+
+    public ListNode detectCyclePosition(ListNode head) {
+        int length = 0;
+
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                length = lengthCycle(slow);
+                break;
+            }
+        }
+
+        if (length == 0) {
+            return null;
+        }
+
+        // place the second pointer s at length position, from where the f and s are at
+        // equal distance
+        ListNode f = head;
+        ListNode s = head;
+
+        while (length > 0) {
+            s = s.next;
+            length--;
+        }
+
+        // keep moving both forward and they will meet at cycle start
+        while (f != s) {
+            f = f.next;
+            s = s.next;
+        }
+        return s;
+    }
 }
--- src/Striver/Module6/LinkedListMedium.java
+++ src/Striver/Module6/LinkedListMedium.java
@@ -97,4 +97,62 @@ public class LinkedListMedium {
         }
         return false;
     }
+
+    public ListNode detectCycle(ListNode head) {
+        int length = 0;
+
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                length = lengthCycle(slow);
+                break;
+            }
+        }
+
+        if (length == 0) {
+            return null;
+        }
+
+        // place the second pointer s at length position, from where the f and s are at
+        // equal distance
+        ListNode f = head;
+        ListNode s = head;
+
+        while (length > 0) {
+            s = s.next;
+            length--;
+        }
+
+        // keep moving both forward and they will meet at cycle start
+        while (f != s) {
+            f = f.next;
+            s = s.next;
+        }
+        return s;
+    }
+
+    public int lengthCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                // calculate the length
+                ListNode temp = slow;
+                int length = 0;
+                do {
+                    temp = temp.next;
+                    length++;
+                } while (temp != slow);
+                return length;
+            }
+        }
+        return 0;
+    }
 }
