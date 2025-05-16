38b2af5a034b6ee5410e63af843227489027a86d
Avish <avitmr2345@gmail.com>
2025-05-16 19:53:20 +0530

    Remove Nth node from the back of the LL

--- src/Striver/Module6/LinkedListMedium.java
+++ src/Striver/Module6/LinkedListMedium.java
@@ -194,4 +194,29 @@ public class LinkedListMedium {
 
         return head;
     }
+
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        int length = 0;
+        ListNode temp = head;
+        while (temp != null) {
+            length++;
+            temp = temp.next;
+        }
+        if (length <= 1) {
+            return null;
+        }
+
+        if (n == length) {
+            return head.next;
+        }
+
+        temp = head;
+        for (int i = 1; i < length - n; i++) {
+            temp = temp.next;
+        }
+
+        temp.next = temp.next.next;
+
+        return head;
+    }
 }
