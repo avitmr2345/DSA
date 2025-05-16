0bba19d37412766d6ea5d0cfe9fbb549afdecf02
Avish <avitmr2345@gmail.com>
2025-05-16 19:57:26 +0530

    Delete the middle node of LL

--- src/Striver/Module6/LinkedListMedium.java
+++ src/Striver/Module6/LinkedListMedium.java
@@ -219,4 +219,23 @@ public class LinkedListMedium {
 
         return head;
     }
+
+    public ListNode deleteMiddle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        ListNode prev = null;
+
+        if (head == null || head.next == null) {
+            return null;
+        }
+
+        while (fast != null && fast.next != null) {
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        prev.next = slow.next;
+
+        return head;
+    }
 }
