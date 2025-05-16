d5af05f995bfb188b24b36dd4305f14a06c63368
Avish <avitmr2345@gmail.com>
2025-05-16 18:50:29 +0530

    Segrregate odd and even nodes in LL

--- src/Striver/Module6/LinkedListMedium.java
+++ src/Striver/Module6/LinkedListMedium.java
@@ -173,4 +173,25 @@ public class LinkedListMedium {
 
         return head == null || headSecond == null;
     }
+
+    public ListNode oddEvenList(ListNode head) {
+
+        if (head == null)
+            return head;
+
+        ListNode odd = head;
+        ListNode even = head.next;
+        ListNode evenHead = even;
+
+        while (even != null && even.next != null) {
+            odd.next = odd.next.next;
+            even.next = even.next.next;
+            odd = odd.next;
+            even = even.next;
+        }
+
+        odd.next = evenHead;
+
+        return head;
+    }
 }
