875a9e7d319700764f497aee0dd0b51f6ea81e11
Avish <avitmr2345@gmail.com>
2025-05-28 15:18:42 +0530

    Insertion sort

--- src/Striver/Module2/Sorting.java
+++ src/Striver/Module2/Sorting.java
@@ -4,9 +4,10 @@ import java.util.Arrays;
 
 public class Sorting {
     public static void main(String[] args) {
-        int[] arr = { 4, 5, 1, 2, 3 };
+        int[] arr = { 5, 3, 4, 1, 2 };
         // selectionSort(arr);
-        bubbleSort(arr);
+        // bubbleSort(arr);
+        insertionSort(arr);
         System.out.println(Arrays.toString(arr));
     }
 
@@ -44,6 +45,22 @@ public class Sorting {
 
             if (!swapped) {
                 break;
+            }
+        }
+    }
+
+    public static void insertionSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j > 0; j--) {
+
+                if (arr[j] < arr[j - 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j - 1];
+                    arr[j - 1] = temp;
+                } else {
+                    break;
+                }
+
             }
         }
     }
