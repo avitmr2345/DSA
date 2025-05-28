eee6f4146e7186bd19b1c362230021d0bd1fd429
Avish <avitmr2345@gmail.com>
2025-05-28 14:51:08 +0530

    Bubble sort

--- src/Striver/Module2/Sorting.java
+++ src/Striver/Module2/Sorting.java
@@ -5,7 +5,8 @@ import java.util.Arrays;
 public class Sorting {
     public static void main(String[] args) {
         int[] arr = { 4, 5, 1, 2, 3 };
-        selectionSort(arr);
+        // selectionSort(arr);
+        bubbleSort(arr);
         System.out.println(Arrays.toString(arr));
     }
 
@@ -24,6 +25,26 @@ public class Sorting {
             int temp = arr[max];
             arr[max] = arr[last];
             arr[last] = temp;
+        }
+    }
+
+    public static void bubbleSort(int[] arr) {
+        boolean swapped;
+        for (int i = 0; i < arr.length; i++) {
+            swapped = false;
+
+            for (int j = 1; j < arr.length - i; j++) {
+                if (arr[j] < arr[j - 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j - 1];
+                    arr[j - 1] = temp;
+                    swapped = true;
+                }
+            }
+
+            if (!swapped) {
+                break;
+            }
         }
     }
 }
