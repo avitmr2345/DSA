52e60f24a3f15b951f47c43c7366673f1bdb6b45
Avish <avitmr2345@gmail.com>
2025-06-05 20:36:45 +0530

    fixed an error

--- src/Striver/Module5/StringsEasy.java
+++ src/Striver/Module5/StringsEasy.java
@@ -50,7 +50,7 @@ class StringsEasy {
     }
 
     public static String largestOddNumber(String num) {
-        for (int i = num.length() - 1; i > 0; i--) {
+        for (int i = num.length() - 1; i >= 0; i--) {
             int n = Character.getNumericValue(num.charAt(i));
             if (n % 2 != 0) {
                 return num;
